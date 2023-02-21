3bbe5a30b06629d83fa3556712349569d356b021
Vyacheslav <vya.polyanskiy@gmail.com>
2023-02-21 16:54:42 +0300

    add class for dinamic array and vector

--- cinema.h
+++ cinema.h
@@ -1,5 +1,8 @@
+#pragma once
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 struct Date
 {
@@ -85,7 +88,7 @@ struct Cinema
     void show_info(){
         std::cout << "Name of cinema: " << cinema_name << "\nName of film: " << film_name;
         std::cout << "\nDate is: " << date_film << "\nTime is: " << time_of_start;
-        std::cout << "\nCost of film is: " << cost;
+        std::cout << "\nCost of film is: " << cost << '\n';
     }
 };
 
@@ -134,6 +137,95 @@ struct City_cinema
         }
     }
         
+};
+
+struct City_cinema_d
+{
+    int cur_len;
+    int max_len;
+    Cinema *lst_cinema;
+
+    City_cinema_d(){
+        this->cur_len = 0;
+        this->max_len = 100;
+        this->lst_cinema = new Cinema[100];
+    }
+    void insert(Cinema c){
+        int tmp;
+        if (cur_len == max_len){
+            Cinema *new_cinema = new Cinema[max_len*2];
+            max_len *= 2;
+            for (int i = 0; i < cur_len; ++i){
+                new_cinema[i] = lst_cinema[i];
+            }
+            lst_cinema = new_cinema;
+        }
+        for (int i = 0; i < cur_len; ++i){
+            if (c.cinema_name == lst_cinema[i].cinema_name){
+                tmp = i;
+                break;
+            }
+        }
+        for (int i = cur_len; i >= tmp; --i)    lst_cinema[i] = lst_cinema[i - 1];
+        lst_cinema[tmp] = c;
+        ++cur_len;
+        
+    }
+    void delete_cinema(Date d){
+        int k = 0, j = 0;
+        for (int i = 0; i < cur_len; ++i){
+            if (lst_cinema[i].date_film == d){
+                k = (k == 0) ? i : k;
+                j = i;
+            }
+        }
+        --j;
+        for (int p = k; p <= j; ++p) lst_cinema[p] = lst_cinema[p + 1];
+        cur_len -= (j - k);
+    }
+
+    void find_cinema(std::string name_film){
+        for (int i = 0; i < cur_len; ++i){
+            if (lst_cinema[i].film_name == name_film)   std::cout << lst_cinema[i].cinema_name << ' ';
+        }
+    }
+};
+bool equel_name_film(Cinema a, Cinema b){
+    return a.cinema_name == b.cinema_name;
+}
+
+bool equel_date(Cinema a, Cinema b){
+    return a.date_film == b.date_film;
+}
+struct City_cinema_vector
+{
+    std::vector <Cinema> cinema_city;
+    void insert(Cinema c){
+        auto it = lower_bound(cinema_city.begin(), cinema_city.end(), c, equel_name_film);
+        cinema_city.insert(it, c);
+        //std::cout << "work\n";
+    }
+
+    void delete_cinema(Date d){
+        Cinema c;
+        c.date_film = d;
+        auto it = lower_bound(cinema_city.begin(), cinema_city.end(), c, equel_date);
+        
+    }
+
+    void show_cinema_name(std::string s){
+        for (auto a: cinema_city){
+            if (a.film_name == s){
+                a.show_info();
+            }
+        }
+    }
+    void show(){
+        //support method
+        for (auto a : cinema_city){
+            a.show_info();
+        }
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "cinema.h"
 
 int main(){
+    /*
     Cinema c;
     c = c.create_film();
     c.show_info();
     City_cinema test;
     test.insert(c);
     test.find_cinema("Avatar");
+    */
+    Cinema c;
+    c = c.create_film();
+    City_cinema_vector tmp;
+    tmp.insert(c);
+    tmp.show();
     return 0;
 }
